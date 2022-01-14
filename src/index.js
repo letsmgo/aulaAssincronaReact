0d58f528e483c125723746b74ca630bb29ea4e24
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-14 14:52:31 -0300

    Demonstrar components com filhos

--- src/index.js
+++ src/index.js
@@ -2,15 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+import ComFilhos from './components/ComFilhos'
+
+/* 
+COMENTADO PARA FINS DIDÁTICOS
 import Primeiro from './components/Primeiro'
 import ComParametro from './components/ComParametro'
+*/
 
 ReactDOM.render(
-    <div>
+   <div>
+       <ComFilhos>
+           <ul>
+               <li>Jao</li>
+               <li>Ze</li>
+               <li>Quim</li>
+               <li>Neto</li>
+           </ul>
+       </ComFilhos>
+   </div>
+    /*
+   COMENTADO PARA FINS DIDÁTICOS
          <Primeiro></Primeiro>
          <ComParametro titulo="Estudo de React" 
          subtitulo="Para iniciantes"/>
-    </div>
-   
+   */
+    
     ,document.getElementById('root')
 )
