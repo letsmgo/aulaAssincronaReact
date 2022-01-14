a2b6305b8260272825a0fbe04ba05b161398e97f
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-14 14:44:50 -0300

    Componente com props

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
+
 ReactDOM.render(
-    <h1>Hello react</h1>
+    <div>
+         <Primeiro></Primeiro>
+         <ComParametro titulo="Estudo de React" 
+         subtitulo="Para iniciantes"/>
+    </div>
+   
     ,document.getElementById('root')
 )
